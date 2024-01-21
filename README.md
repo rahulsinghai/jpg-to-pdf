845e92d074fc1a8ac738dd1485623bb9846a7120
rsinghai <rsinghai@tripadvisor.com>
2024-01-21 23:59:04 +0000

    Added defaults

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
+# Jpg to PDF utility
+
 Configure border and image quality (to restrict output PDF of a particular size) in the source code.
 
 ```shell
 python3 -m venv venv
 source venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
-python src/JpgToPdf.py
+python src/JpgToPdf.py <input_image_path> <output_pdf_path> <image_quality> <border_size>
 deactivate
 ```
 
 ## Gitpod
 
+With Gitpod, you spin up fresh, automated dev environments for each task, in the cloud, in seconds.
+
 1. Commit your source code to Github/Gitlab/BitBucket.
-2. Create a GitPod workspace
-3. `gp init`
+2. Create a GitPod workspace by prefixing Repo URL by https://gitpod.io/#
+3. `gp init`. This will auto-create a `.gitpod.yml` file by auto-detecting project type.
+4. Run `python src/JpgToPdf.py <input_image_path> <output_pdf_path> <image_quality> <border_size>`. All 4 parameters are optional.
--- src/JpgToPdf.py
+++ src/JpgToPdf.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image
 from reportlab.pdfgen import canvas
 
@@ -8,17 +9,29 @@ def compress_image(orig_image_path, output_image_path, quality=85):
 
 
 if __name__ == "__main__":
-    # Replace 'input.jpg' with the path to your JPG file
-    input_image_path = 'original_input.jpg'
+    # Default values
+    input_image_path: str = 'resources/original_input.jpg'
+    output_pdf_path: str = 'resources/output.pdf'
+    image_quality: int = 85
+    border_size: int = 344  # Set the border size (1 inch = 72 points)
 
-    # Replace 'output.pdf' with the desired output PDF file path
-    output_pdf_path = 'output.pdf'
+    # Check if parameters are provided
+    if len(sys.argv) > 1:
+        input_image_path = sys.argv[1]
+    if len(sys.argv) > 2:
+        output_pdf_path = sys.argv[2]
+    if len(sys.argv) > 3:
+        image_quality = int(sys.argv[3])
+    if len(sys.argv) > 4:
+        border_size = int(sys.argv[4])
+
+    print(f"Using input file: {input_image_path}")
+    print(f"Will output to: {output_pdf_path}")
+    print(f"Image quality: {image_quality}%")
+    print(f"Border size of : {border_size} points")
 
     # Open the JPG image using Pillow
     img = Image.open(input_image_path)
-
-    # Set the border size (1 inch = 72 points)
-    border_size = 344
 
     # Calculate the dimensions of the PDF with the border
     pdf_width = img.width + 2 * border_size
