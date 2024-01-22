73d26d556f183e546fa03aa7febb1db211f087d2
rsinghai <rsinghai@tripadvisor.com>
2024-01-22 00:11:35 +0000

    Output files now go to same folder of input file

--- src/JpgToPdf.py
+++ src/JpgToPdf.py
@@ -1,17 +1,53 @@
+import os
 import sys
 from PIL import Image
 from reportlab.pdfgen import canvas
 
 
-def compress_image(orig_image_path, output_image_path, quality=85):
+def compress_image(orig_image_path, compressed_image_path, image_quality=85):
     orig_img = Image.open(orig_image_path)
-    orig_img.save(output_image_path, quality=quality)
+    orig_img.save(compressed_image_path, quality=image_quality)
+
+    return compressed_image_path
+
+
+def convert_jpg_to_pdf(input_image_path, compressed_image_path, output_pdf_path, image_quality, border_size):
+    # Open the JPG image using Pillow
+    img = Image.open(input_image_path)
+
+    # Calculate the dimensions of the PDF with the border
+    pdf_width = img.width + 2 * border_size
+    pdf_height = img.height + 2 * border_size
+
+    # Create a PDF file with the calculated dimensions
+    pdf_canvas = canvas.Canvas(output_pdf_path, pagesize=(pdf_width, pdf_height))
+
+    # Compress the image before drawing onto the PDF
+    compressed_image_path = compress_image(input_image_path, compressed_image_path, image_quality)
+
+    # Save the current state of the canvas
+    pdf_canvas.saveState()
+
+    # Set the compression quality
+    pdf_canvas.drawImage(compressed_image_path, border_size, border_size,
+                         width=img.width, height=img.height, preserveAspectRatio=True, mask='auto')
+
+    # Restore the previous state of the canvas
+    pdf_canvas.restoreState()
+
+    # Save the PDF file
+    pdf_canvas.save()
 
 
 if __name__ == "__main__":
     # Default values
     input_image_path: str = 'resources/original_input.jpg'
-    output_pdf_path: str = 'resources/output.pdf'
+
+    # Generate default output PDF file path
+    base_path, file_name = os.path.split(input_image_path)
+    file_name_without_extension, extension = os.path.splitext(file_name)
+    output_pdf_path = os.path.join(base_path, f"{file_name_without_extension}.pdf")
+
     image_quality: int = 85
     border_size: int = 344  # Set the border size (1 inch = 72 points)
 
@@ -30,31 +66,7 @@ if __name__ == "__main__":
     print(f"Image quality: {image_quality}%")
     print(f"Border size of : {border_size} points")
 
-    # Open the JPG image using Pillow
-    img = Image.open(input_image_path)
-
-    # Calculate the dimensions of the PDF with the border
-    pdf_width = img.width + 2 * border_size
-    pdf_height = img.height + 2 * border_size
-
-    # Create a PDF file with the calculated dimensions
-    pdf_canvas = canvas.Canvas(output_pdf_path, pagesize=(pdf_width, pdf_height))
-
     # Path to the compressed image
-    compressed_image_path = 'compressed_input.jpg'
+    compressed_image_path = os.path.join(base_path, f"{file_name_without_extension}_compressed{extension}")
 
-    # Compress the image before drawing onto the PDF
-    compress_image(input_image_path, compressed_image_path, quality=78)
-
-    # Save the current state of the canvas
-    pdf_canvas.saveState()
-
-    # Set the compression quality
-    pdf_canvas.drawImage(compressed_image_path, border_size, border_size,
-                         width=img.width, height=img.height, preserveAspectRatio=True, mask='auto')
-
-    # Restore the previous state of the canvas
-    pdf_canvas.restoreState()
-
-    # Save the PDF file
-    pdf_canvas.save()
+    convert_jpg_to_pdf(input_image_path, compressed_image_path, output_pdf_path, image_quality, border_size)
